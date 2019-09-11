4df463a15d42927881eb16ef73bfef39267c94cd
Diego <diego.f.barmor@hotmail.com>
2019-09-11 14:02:45 -0500

    Update EjemploRoomies.js

--- EjemploRoomies.js
+++ EjemploRoomies.js
@@ -1,1 +1,8 @@
-let hambre = process.argv
+let Diego = parseFloat("Que nivel eres? " , "Escribe tu nivel prro");
+
+if (Diego ===  0 ) {
+    console.log("Aprendiz");
+} else{
+    console.log("Eso no existe man ");
+    
+}
